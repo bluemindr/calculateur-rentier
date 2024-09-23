23864160169410295885e04fadf6589742737361
Cédric Chanal <cedric.advice@gmail.com>
2024-09-23 11:21:14 +0200

    add wealth tax and fix computation

--- app_calculateur.py
+++ app_calculateur.py
@@ -2,31 +2,40 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Définition des fonctions pour les calculs
-def update_income(current_income, rate_increase, age, retirement_age, state_retirement_age, state_pension):
+def compute_income(current_income, age, retirement_age, state_retirement_age, state_pension):
     income = 0
     if age >= state_retirement_age:
         income += state_pension
 
     if age >= retirement_age:
-        return income  # Pas de revenu du travail après la retraite
+        return income
     else:
-        return current_income * (1 + rate_increase) + income
+        # add updated work income
+        return current_income + income
+
+def update_income(current_income, rate_increase, age, retirement_age):
+    if age <= retirement_age:
+        return current_income *(1+rate_increase)
+    return 0
 
 def update_spending(current_spending, inflation_rate):
     return current_spending * (1 + inflation_rate)
 
-def update_net_worth(net_worth, income, spending, return_rate, age, retirement_age, investment_tax_rate):
-    if age > retirement_age:
-        spending = spending/(1 - investment_tax_rate)
-    new_worth = net_worth * (1 + return_rate) + income - spending
+def update_net_worth(net_worth, income, spending, return_rate, age, investment_tax_rate, wealth_tax_rate, wealth_tax_threshold):
+    wealth_tax = 0.
+    if net_worth > wealth_tax_threshold:
+        wealth_tax = (net_worth-wealth_tax_threshold)*wealth_tax_rate
+    s = spending + wealth_tax - income
+    if s > 0:
+        s = s/(1-investment_tax_rate)
+    new_worth = net_worth * (1 + return_rate) - s
     return max(new_worth, 0)
 
-# Création de la fonction principale pour gérer la simulation
 def retirement_simulation(start_age, end_age, current_income, income_increase_rate,
-                          current_spending, inflation_rate, initial_net_worth, investment_return_rate, retirement_age, investment_tax_rate, state_pension, state_retirement_age):
+                          current_spending, inflation_rate, initial_net_worth,
+                          investment_return_rate, retirement_age, investment_tax_rate,
+                          state_pension, state_retirement_age, wealth_tax_rate, wealth_tax_threshold):
 
-    years = end_age - start_age
     age_range = range(start_age, end_age + 1)
     data = {
         "Age": age_range,
@@ -38,37 +47,35 @@ def retirement_simulation(start_age, end_age, current_income, income_increase_ra
 
     net_worth = initial_net_worth
     for age in age_range:
-        if age > start_age:
-            state_pension = state_pension*(1+inflation_rate)
-            current_income = update_income(current_income, income_increase_rate, age, retirement_age, state_retirement_age, state_pension)
-            current_spending = update_spending(current_spending, inflation_rate)
-
-        new_net_worth = update_net_worth(net_worth, current_income, current_spending, investment_return_rate, age, retirement_age, investment_tax_rate)
-
-        # Enregistre les données pour l'année
         data["Income"].append(current_income)
         data["Spending"].append(current_spending)
-        data["Net Worth"].append(new_net_worth)
-        data["Delta Net Worth"].append(new_net_worth - net_worth)
+        data["Net Worth"].append(net_worth)
 
-        # Mise à jour du patrimoine net pour l'année suivante
+        current_income = compute_income(current_income, age, retirement_age, state_retirement_age, state_pension)
+        new_net_worth = update_net_worth(net_worth, current_income, current_spending, investment_return_rate, age, investment_tax_rate, wealth_tax_rate, wealth_tax_threshold)
+        # def apply inflation and actualization
+        state_pension = state_pension*(1+inflation_rate)
+        current_spending = update_spending(current_spending, inflation_rate)
+        current_income = update_income(current_income, income_increase_rate, age, retirement_age)
+        data["Delta Net Worth"].append(new_net_worth - net_worth)
         net_worth = new_net_worth
 
     df = pd.DataFrame(data)
+    df["Net Worth (euros constants)"] = df["Net Worth"]/(1+inflation_rate)**(df["Age"]-start_age)
     return df
 
 # Configuration de l'interface Streamlit
 st.title("Calculateur de rente avancé")
 
 INIT_AGE = 41
-RETIREMENT_AGE = 45
+RETIREMENT_AGE = 41
 CURRENT_INCOME = 48_000
 INCOME_INCREASE_RATE = 0.022
 CURRENT_SPENDING = 45_000
-INFLATION = 0.04
-INIT_NET_WORTH = 1_300_000
-RETURN_RATE = 0.06
-INVESTMENT_TAX_RATE = 0.3
+INFLATION = 0.03
+INIT_NET_WORTH = 1_680_000
+RETURN_RATE = 0.07
+INVESTMENT_TAX_RATE = 0.33
 STATE_PENSION = 8900
 STATE_RETIREMENT_AGE = 66
 
@@ -83,7 +90,15 @@ current_spending = st.number_input("Dépenses annuelles", min_value=0, value=CUR
 inflation_rate = st.number_input("Taux d'inflation", min_value=0.0, max_value=1.0, format=percent_format, value=INFLATION)
 initial_net_worth = st.number_input("Patrimoine initial", min_value=0, value=INIT_NET_WORTH)
 investment_return_rate = st.number_input("Taux de rendement des placements", format=percent_format, min_value=-1.0, max_value=1.0, value=RETURN_RATE)
-investment_tax_rate = st.number_input("Taxe sur les plus values réalisées", format=percent_format, min_value=0.0, max_value = 1.0, value = 0.3)
+investment_tax_rate = st.number_input("Taxe sur les plus values réalisées", format=percent_format, min_value=0.0, max_value = 1.0, value = INVESTMENT_TAX_RATE)
+
+is_wealth_tax = st.checkbox("ISF", value = False)
+wealth_tax_rate = 0
+wealth_tax_threshold = 0
+if is_wealth_tax:
+    wealth_tax_rate = st.number_input("ISF", format=percent_format, min_value = 0., max_value = 1., value = 0.)
+    wealth_tax_threshold = st.number_input("seuil ISF", min_value = 0, step= 100000, value = 1_000_000)
+
 
 is_pension = st.checkbox("Retraite de l'état", value = False)
 
@@ -97,8 +112,9 @@ else:
 # Calcul et affichage des résultats
 if st.button("Calculer la rente"):
     df = retirement_simulation(start_age, end_age, current_income, income_increase_rate,
-                               current_spending, inflation_rate, initial_net_worth, investment_return_rate, retirement_age, investment_tax_rate,
-                               state_pension, state_retirement_age)
+                               current_spending, inflation_rate, initial_net_worth,
+                               investment_return_rate, retirement_age, investment_tax_rate,
+                               state_pension, state_retirement_age, wealth_tax_rate, wealth_tax_threshold)
     
     st.write("Résultats de la simulation")
     st.dataframe(df)
@@ -121,7 +137,6 @@ if st.button("Calculer la rente"):
     st.pyplot(fig)
 
     # Graphique de l'évolution du patrimoine à euros constants
-    df["Net Worth (euros constants)"] = df["Net Worth"]/(1+inflation_rate)**(df["Age"]-start_age)
     fig, ax = plt.subplots()
     ax.bar(df["Age"],df["Net Worth (euros constants)"] , color='blue')
     ax.set_xlabel("Age")
